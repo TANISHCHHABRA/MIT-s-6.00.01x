85072ca05fec3d9041133e50eb5bed1cb7e8005c
TANISH <52378401+TANISHCHHABRA@users.noreply.github.com>
2019-10-25 19:44:37 +0530

    Created Comments

--- Week 1/Problem Set 1/Problem 3/Problem 3.py
+++ Week 1/Problem Set 1/Problem 3/Problem 3.py
@@ -1,10 +1,15 @@
 longest = ''
 current = ''
+# Iterate over the given string
 for i in range(len(s)):
+    # ord is used for getting ASCII value of the character
+    # Check whether the next alphabet is greater than the present alphabet and update the current string
     if ord(s[i]) >= ord(s[i-1]):
         current += s[i]
+    # If the next alphabet is smaller then the present alphabet reset the current string from present character
     else:
         current = s[i]
+    # Updating the longest string
     if len(current) > len(longest):
         longest = current
 print(longest)
