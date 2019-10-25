7636a0d4bcf76bb7aad796b8ca9d7764490353f2
TANISH <52378401+TANISHCHHABRA@users.noreply.github.com>
2019-10-25 19:38:14 +0530

    Created Comments

--- Week 1/Problem Set 1/Problem 1/Problem 1.py
+++ Week 1/Problem Set 1/Problem 1/Problem 1.py
@@ -1,6 +1,8 @@
 vowels=['a','e','i','o','u']
 count = 0
+# Iterate over every charcter in given string
 for letter in s:
+    # If character is a vowel
     if letter in vowels:
         count += 1
 print(count)
