be77e5de63e64e69a954176ccbc0b7a359f7a2de
TANISH <52378401+TANISHCHHABRA@users.noreply.github.com>
2019-10-30 19:16:07 +0530

    Update Grader.py

--- Week 2/Grader/Grader.py
+++ Week 2/Grader/Grader.py
@@ -1,7 +1,19 @@
+# Tanish
+
+# Import Tan and pi function from math library
 from math import tan,pi
 
+# Define a function Polysum
 def polysum(n, s):
+    
+    # Compute the area off the polygon
     area = (0.25*n*(s**2))/tan(pi/n)
+    
+    # Compute perimeter of polygon
     perimeter = n * s
+    
+    # Compute the sum of area and square of perimeter
     sum = area + (perimeter**2)
-    return(round(sum, 4))
+    
+    # Return the sum by rounding it to 4 decimal numbers
+    return(sum)
