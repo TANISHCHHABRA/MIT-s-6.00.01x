6c48236bedc27976dfd37185a07b50aaea3aa56a
TANISH <52378401+TANISHCHHABRA@users.noreply.github.com>
2019-10-30 17:43:25 +0530

    Update Guess.py

--- Week 2/Guess my Number/Guess.py
+++ Week 2/Guess my Number/Guess.py
@@ -3,13 +3,18 @@ l = 0
 h = 100
 
 while True :
+    # Changes the avg value
     avg = (l + h)//2
+    # Ask the user whether the guess is high, low or correct
     print("Is your secret number " ,avg,"?")
     ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+    # If the guess is high
     if ans =='h' :
         h = avg
+    # If the guess is low
     elif ans == 'l' :
         l = avg
+    # If the guess is correct
     elif ans == 'c' :
         print("Game over. Your secret number was: " , avg)
         break
